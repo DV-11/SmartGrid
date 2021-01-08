fb60c3a4b011a3bda282cdd3025951bfd45bd0ab
Storm Hartkamp <stormhartkamp@live.nl>
2021-01-08 17:06:08 +0100

    improved

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ if __name__ == "__main__":
     # Create a grid from our data
     grid = grid.Grid(f"data/{district}/{district}_batteries.csv", f"data/{district}/{district}_houses.csv")
 
+    # temp
+    with open('listfile.txt', 'w') as f:
+        for Battery in grid.all_batteries:
+            f.write(Battery.x_coordinate, Battery.y_coordinate)
+        for House in grid.all_houses:
+            f.write()
     
