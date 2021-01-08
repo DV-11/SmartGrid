c187d43edbf869dd1883925ffd00389ba183d87c
Steash <steashedits@gmail.com>
2021-01-08 15:21:02 +0100

    updated main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from code.classes import grid, battery, house
+from code.algorithms import randomize
+from code.algorithms import greedy as gr
+from code.visualization import visualize as visualization
+
+if __name__ == "__main__":
+
+    # Create a grid from our data
+    
