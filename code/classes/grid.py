196f37195a19522b6c61f2954296fb0e3016065a
Storm Hartkamp <stormhartkamp@live.nl>
2021-01-08 15:02:15 +0100

    added loading functions to grid class

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,13 +1,31 @@
+from .house import House
+from .battery import Battery
+
 class Grid():
-    def __init__(self, source_file):
-        self.size = pass
+    def __init__(self, house_file, battery_file, size):
+        self.size = size
         self.all_batteries = []
         self.all_houses = []
+        self.load_batteries(house_file)
+        self.load_houses(battery_file)
 
     def load_batteries(self, source_file):
+        with open (source_file) as f:
+            # Creates batteries and saves them in a list
+            line = f.readline()
+            while line != '\n':
+                line = line.split(',')
+                new_battery = Battery(line[0], line[1], line[2])
+                self.all_batteries.append(new_battery)
+                f.readline()
 
-        batteries = {}
-        with open(source_file, 'r') as in_file:
-            reader = csv.DictReader(in_file)
-
+    def load_houses(self, source_file):
+        with open (source_file) as f:
+            # Creates houses and saves them in a list
+            line = f.readline()
+            while line != '\n':
+                line = line.split(',')
+                new_house = House(line[0], line[1], line[2])
+                self.all_batteries.append(new_house)
+                f.readline()
             
