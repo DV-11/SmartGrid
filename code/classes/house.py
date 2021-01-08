6b3774deab28a4874ee2beb6c1b58ee6ab02b8a1
Storm Hartkamp <stormhartkamp@live.nl>
2021-01-08 13:01:21 +0100

    added battery and house class

--- code/classes/house.py
+++ code/classes/house.py
@@ -1,7 +1,8 @@
 import csv
 
 class House():
-    def __init__(self):
-        
-        # TODO
-        pass
+    def  __init__(self, x, y, output):
+        self.id = 0
+        self.x_coordinate = x
+        self.y_coordinate = y
+        self.output = output
