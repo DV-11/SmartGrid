c7cd165d8d50d806003359ff58d5c03d9aad60e1
DV-11 <46575712+DV-11@users.noreply.github.com>
2021-01-08 11:17:42 +0100

    Update visualize.py

--- code/visualization/visualize.py
+++ code/visualization/visualize.py
@@ -1,1 +1,22 @@
-# TODO
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Grid():
+    def __init__(self):
+        
+        c = [(38,12),(43,13),(42,3),(49,23),(3,45)]
+
+        x_c = [i[0] for i in c]
+        y_c = [i[1] for i in c]
+
+
+
+        plt.scatter(x_c, y_c)
+        plt.xticks(np.arange(0, 51, 10))
+        plt.yticks(np.arange(0, 51, 10))
+        plt.minorticks_on()
+
+        plt.grid(which='major')
+        plt.grid(which='minor', alpha = 0.25)
+        plt.show()
