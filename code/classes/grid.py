bd0eb5f7e47778097bfdf0465d305821b0c165c6
Steash <steashedits@gmail.com>
2021-01-08 14:43:53 +0100

    updated datastruct

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,5 +1,13 @@
 class Grid():
-    def __init__(self):
-        
-        # TODO
-        pass
+    def __init__(self, source_file):
+        self.size = pass
+        self.all_batteries = []
+        self.all_houses = []
+
+    def load_batteries(self, source_file):
+
+        batteries = {}
+        with open(source_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
+
+            
