98dc77c30dcd7a4a82f6e9f5876cbc212dc3b7dc
Storm Hartkamp <stormhartkamp@live.nl>
2021-01-08 14:31:14 +0100

    added function to battery

--- code/classes/battery.py
+++ code/classes/battery.py
@@ -8,3 +8,11 @@ class Battery():
         self.capacity = capacity
         self.current_capacity = capacity
         self.cables = []
+        self.houses = []
+    
+    def add_house(self, house):
+        self.houses.append(house)
+        self.current_capacity = self.current_capacity - house.output
+
+    def add_cable(self, cable):
+        self.cables.append(cable)
