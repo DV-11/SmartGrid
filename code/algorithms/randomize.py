5fc7a0f264d01c1dcf57a09fd300fcd1ab535ed0
DV-11 <david.valero.m@gmail.com>
2021-01-12 21:31:30 +0100

    randomize algorythm

--- code/algorithms/randomize.py
+++ code/algorithms/randomize.py
@@ -8,26 +8,37 @@ def random_assignment(grid):
     return grid
 
 def create_cable(house, battery):
-    # Sets cable to right y-coordinate
-    latest_y = house.y_coordinate
-    latest_x = house.x_coordinate
+    # set coordinates to ints 
+    battery_c = [int(battery.x_coordinate),int(battery.y_coordinate)]
+    house_c = [int(house.x_coordinate),int(house.y_coordinate)]
 
-    while latest_y < battery.y_coordinate:
-        latest_y -= 1
-        new_cable = (latest_x, latest_y)
-        house.cables.append(new_cable)
+    # first points where the cable goes through 
+    latest = house_c
 
-    while latest_y > battery.y_coordinate:
-        latest_y += 1
-        new_cable = (latest_x, latest_y)
-        house.cables.append(new_cable)
-
-    while latest_x < battery.x_coordinate:
-        latest_x -= 1
-        new_cable = (latest_x, latest_y)
-        house.cables.append(new_cable)
-
-    while latest_x > battery.x_coordinate:
-        latest_x += 1
-        new_cable = (latest_x, latest_y)
-        house.cables.append(new_cable)
+    # determine which points in the grid the cable goes through 
+    if house_c[1] >= battery_c[1]:
+        while latest[1] > battery_c[1]:
+    
+            house.cables.append(tuple(latest))
+            latest[1] = latest[1]-1
+    
+    else:
+        while latest[1] < battery_c[1]:
+    
+            house.cables.append(tuple(latest))
+            latest[1] = latest[1]+1
+        
+    if house_c[0] >= battery_c[0]:
+        while latest[0] > battery_c[0]:
+        
+            house.cables.append(tuple(latest))
+            latest[0] = latest[0] - 1
+    
+    else:
+        while latest[0] < battery_c[0]:
+        
+            house.cables.append(tuple(latest))
+            latest[0] = latest[0] + 1
+        
+    # final destination of the cable
+    house.cables.append(tuple(battery_c))
