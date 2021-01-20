e0b4c4c964a78ea49394004d29a3a723eef26832
Storm Hartkamp <stormhartkamp@live.nl>
2021-01-20 15:23:24 +0100

    added comments

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -13,9 +13,15 @@ class hillclimber():
         pass
 
     def find_to_mutate(self, grid):
+        """
+        Returns houses with longest distance to their destination along with a few random ones
+        """
+        # Sorts all houses based on distance
         houses_list = list(grid.all_houses.values())
         houses_list = sorted(houses_list, key=lambda House:House.distance, reverse=True)
         to_change = []
+
+        # Picks houses with longest disance and a few random houses to change
         for i in range (self.houses_to_change):
             to_change.append(houses_list[i])
             random_number = random.randint(self.houses_to_change, len(houses_list))
@@ -25,15 +31,18 @@ class hillclimber():
     def mutate_house_cable(self, houses):
         # delete existing cables
         for House in houses:
-            for i in House.cables:
-                print(i)
-                print(self.best_grid.all_batteries.get(House.battery).cables)
-                self.best_grid.all_batteries.get(House.battery).cables.remove(tuple(i))
+            self.best_grid.all_batteries.get(House.battery).cables = list(set(self.best_grid.all_batteries.get(House.battery).cables))
+            for i in range(len(House.cables)):
+                self.best_grid.all_batteries.get(House.battery).cables.remove(House.cables[i])
+                House.cable.remove(House.cables[i])
+            
+
         # create new cables
         for House in houses:
             self.create_shared_cable(House)
 
     def run(self, grid):
+        self.best_grid = grid
         houses = self.find_to_mutate(grid)
         self.mutate_house_cable(houses)
         return grid
--- code/algorithms/randomize2.py
+++ code/algorithms/randomize2.py
@@ -5,26 +5,40 @@ class randomize_shared():
         self.grid = grid 
 
     def get_destination(self, house):
+        """
+        Finds closest battery or cable from house
+        """
+
         shortest_distance = float('inf')
+
+        # Loops through all batteries and finds all cables connected to them to find nearest destination
         for Battery in self.grid.all_batteries.values():
-           
             battery_cables = list(Battery.cables)
-            for i in range(len(battery_cables)):
-                new_distance = self.get_distance(house.x_coordinate, house.y_coordinate, int(battery_cables[i][0]), int(battery_cables[i][1]))
+
+            for cable in range(len(battery_cables)):
+                new_distance = self.get_distance(house.x_coordinate, house.y_coordinate, 
+                int(battery_cables[cable][0]), int(battery_cables[cable][1]))
+                
+                # Updates shortest distance and saves both coordinates of cable/battery as well as the 
+                # id of the corresponding battery for retrieving it later.
                 if new_distance < shortest_distance:
                     shortest_distance = new_distance
-                    house.destination = tuple([int(battery_cables[i][0]), int(battery_cables[i][1])])
+                    house.destination = tuple([int(battery_cables[cable][0]), int(battery_cables[cable][1])])
                     house.battery = Battery.id
+
         house.distance = self.get_distance(house.x_coordinate, house.y_coordinate, house.destination[0], house.destination[1])
         self.grid.all_batteries.get(house.battery).remaining_capacity -= house.output
         
     def get_distance(self, origin_x, origin_y, destination_x, destination_y):
+        """
+        Finds distance between two points using their coordinates
+        """
         horizontal_distance = int(origin_x) - destination_x
         if horizontal_distance < 0:
-            horizontal_distance = horizontal_distance * -1
+            horizontal_distance *= -1
         vertical_distance = int(origin_y) - destination_y
         if vertical_distance < 0:
-            vertical_distance = vertical_distance * -1
+            vertical_distance *= -1
         return vertical_distance + horizontal_distance
 
     def create_new_cable(self,house):
@@ -47,15 +61,21 @@ class randomize_shared():
                 current_distance = self.get_distance(new_cable[0], new_cable[1], house.destination[0], house.destination[1])
             house.latest_cable = new_cable
             house.cables.append(new_cable)
-            self.grid.all_batteries.get(house.battery).cables.append(new_cable)
+            self.grid.all_batteries.get(house.battery).cables.append(tuple(new_cable))
             current_distance = saved_distance - 1
             
     def run(self, grid):
+        """
+        Randomizes order of all houses and creates a path to a battery or existing cable
+        in a semi-random fashion
+        """
+        # Loop through all houses in grid
         all_keys = list(self.grid.all_houses.keys())
+        # Randomize order of keys
         random.shuffle(all_keys)
-        for i in range(len(all_keys)):
-            self.get_destination(self.grid.all_houses.get(all_keys[i]))
-            self.create_new_cable(self.grid.all_houses.get(all_keys[i]))
+        for key in range(len(all_keys)):
+            self.get_destination(self.grid.all_houses.get(all_keys[key]))
+            self.create_new_cable(self.grid.all_houses.get(all_keys[key]))
         return grid
     
     def calculate_cost(self, grid):
