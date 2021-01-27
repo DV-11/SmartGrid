ceee0ba645eae4c1e642dc7ac521b7d5fddb86f5
Storm Hartkamp <stormhartkamp@live.nl>
2021-01-27 22:05:27 +0100

    improved main.py

--- main.py
+++ main.py
@@ -24,38 +24,42 @@ if __name__ == "__main__":
     grid = grid.Grid(f"data/{district}/{district}_batteries.csv", f"data/{district}/{district}_houses.csv")
     print(f"Loading grid with data from district-{district_number}.")
 
-    # Calculate combined battery capacity and combined house output
-    total_capacity = 0
-    for Battery in grid.all_batteries.values():
-        total_capacity += Battery.capacity
-
-    total_output = 0
-    for House in grid.all_houses.values():
-        total_output += House.output
-
     # Allows user to choose an algorithm
-    all_algorithms = {"random_unique": randomize(grid), "restricted_greedy": restricted_greedy(grid), 
-                        "shared_randomize" : randomize_shared(grid), "hillclimber": hillclimber(grid), 
-                        "simulated_annealing": simulated_annealing(grid, temperature=10000), 
-                        "unique_hillclimber": u_hillclimber(grid)}
+    unique_cables_algorithms = {"randomize": randomize(grid), "greedy": restricted_greedy(grid), "hillclimber": u_hillclimber(grid)}
+    shared_cables_algorithms = {"randomize" : randomize_shared(grid), "hillclimber": hillclimber(grid), "simulated annealing": simulated_annealing(grid, temperature=10000),}
     chosen_algorithm = None
-    while chosen_algorithm not in all_algorithms:
-        chosen_algorithm = input(f"Choose an algorithm {all_algorithms.keys()} \n")
 
+    valid_choices = ["yes", "no"]
+    choice = None
+    while choice not in valid_choices:
+        choice = input("Would you like to run an algorithm where cables can be shared? (yes/no)\n")
+    
+
+    if choice == 'no':
+        chosen_algorithm = input(f"Choose an algorithm {unique_cables_algorithms.keys()} \n")
+        print(f"Running algorithm: {chosen_algorithm}.")
+        chosen_algorithm = unique_cables_algorithms.get(chosen_algorithm)
+    else:
+        chosen_algorithm = input(f"Choose an algorithm {shared_cables_algorithms.keys()} \n")
+        print(f"Running algorithm: {chosen_algorithm}.")
+        chosen_algorithm = shared_cables_algorithms.get(chosen_algorithm)
     # Runs chosen algorithm
-    print(f"Running algorithm: {chosen_algorithm}.")
-    chosen_algorithm = all_algorithms.get(chosen_algorithm)
+    
+    
 
     if isinstance(chosen_algorithm, restricted_greedy):
         grid = restricted_greedy(grid).run(grid, district_number)
+
     elif isinstance(chosen_algorithm, hillclimber):
         to_change = int(input("How many houses would you like to change per iteration (recommend 2-5)\n"))
         iterations = int(input("How many times should the program run without improving before quitting?\n"))
         grid = randomize_shared(grid).run(grid)
         grid = chosen_algorithm.run(grid, to_change, iterations)
+
     elif isinstance(chosen_algorithm, simulated_annealing):
         iterations = int(input("How many times should the program run without improving before quitting?\n"))
         grid = chosen_algorithm.run(grid, iterations)
+
     elif isinstance(chosen_algorithm, u_hillclimber):
         to_change = int(input("How many houses would you like to change per iteration (recommend 2-5)\n"))
         iterations = int(input("How many times should the program run without improving before quitting?\n"))
